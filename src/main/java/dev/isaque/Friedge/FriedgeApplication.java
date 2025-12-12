97b5fcd55435bdedbcbd13e8a7db34b1045155b5
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-12-12 01:26:23 -0300

    Primeiro commit

---
+++ src/main/java/dev/isaque/Friedge/FriedgeApplication.java
@@ -0,0 +1,13 @@
+package dev.isaque.Friedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/isaque/Friedge/FriedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.isaque.Friedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
